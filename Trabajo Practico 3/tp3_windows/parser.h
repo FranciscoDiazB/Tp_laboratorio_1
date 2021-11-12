13cd929445d39d8cccb59f33b1756f70219dab10
FranciscoDiazB <frandiazb97@gmail.com>
2021-11-12 17:37:39 -0300

    TP3 Finalizado

---
+++ Trabajo Practico 3/tp3_windows/parser.h
@@ -0,0 +1,21 @@
+/*
+ * parser.h
+ *
+ *  Created on: 2 nov. 2021
+ *      Author: Asus
+ */
+#include "LinkedList.h"
+#include "Employee.h"
+
+#ifndef PARSER_H_
+#define PARSER_H_
+
+int parser_EmployeeFromText(FILE* pFile , LinkedList* pArrayListEmployee);
+
+int parser_EmployeeFromBinary(FILE* pFile , LinkedList* pArrayListEmployee);
+
+int parser_writeToBinary(FILE* pFile , LinkedList* pArrayListEmployee);
+
+int parser_writeToText(FILE* pFile , LinkedList* pArrayListEmployee);
+
+#endif /* PARSER_H_ */
